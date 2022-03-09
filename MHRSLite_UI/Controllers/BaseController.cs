3f71ba10935550ac64f15e04ef06dd25b50fe417
Şeyda Zenler <seydaazenler@gmail.com>
2022-03-09 09:08:02 +0300

    Google ve Facebook ile giriş yap backend kodları eklendi.

---
+++ MHRSLite_UI/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHRSLite_UI.Controllers
+{
+    public class BaseController : Controller
+    {
+        public void AddModelErrors(IdentityResult result)
+        {
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+        }
+    }
+}
